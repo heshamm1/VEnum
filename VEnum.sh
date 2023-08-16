de0236e0f646f67dbde3b05deeaf22b028a6c332
Hesham Ahmed <87103294+heshamm1@users.noreply.github.com>
2023-08-16 18:06:53 +0300

    Update VEnum.sh

--- VEnum.sh
+++ VEnum.sh
@@ -15,17 +15,18 @@ banner="
 "
 
 usage="
-Usage: $(basename "$0") -l TARGET_FILE [-f FILENAME] [-h] [-v] [--resolve IP_FILE] [--proxy PROXY] [--only TOOL1,TOOL2,...] [-a]
+Usage: $(basename "$0") [-d TARGET | -l FILENAME] [-f FILENAME] [-h] [-v] [--resolve IP_FILE] [--proxy PROXY] [--only TOOL1,TOOL2,...] [-a]
 
 Options:
-  -l       File containing list of target domains (required)
-  -f       Filepath and name to save results (default: subs.txt)
-  -h       Show this help message and exit
-  -v       Verbose output
-  --resolve    Resolve subdomains to IPs and save to specified file
-  --proxy  Use a proxy (format: http://IP:Port)
-  --only   Use specific tools (comma-separated list)
-  -a       Use all available tools (default)
+  -d       	Target domain
+  -l       	File containing a list of target domains (one per line)
+  -f       	Filepath and name to save results (default: subs.txt)
+  -h       	Show this help message and exit
+  -v       	Verbose output
+  --resolve     Resolve subdomains to IPs and save to specified file
+  --proxy  	Use a proxy (format: http://IP:Port)
+  --only   	Use specific tools (comma-separated list)
+  -a       	Use all available tools (default)
 "
 
 # Default values
@@ -35,8 +36,13 @@ verbose=false
 
 while [[ $# -gt 0 ]]; do
   case $1 in
+    -d)
+      target="$2"
+      shift
+      shift
+      ;;
     -l)
-      target_file="$2"
+      list_file="$2"
       shift
       shift
       ;;
@@ -82,51 +88,75 @@ done
 
 echo "$banner"
 
-if [ -z "$target_file" ]; then
-  echo "Error: Target file is required. Use -h for help."
+if [ -z "$target" ] && [ -z "$list_file" ]; then
+  echo "Error: Either a target domain (-d) or a list of target domains (-l) is required. Use -h for help."
   exit 1
 fi
 
 rm -f "$filename"
 touch "$filename"
 
-IFS=$'\r\n' GLOBIGNORE='*' command eval 'targets=($(cat "$target_file"))'
+IFS=',' read -ra tool_list <<< "$tools"
 
-for target in "${targets[@]}"; do
-  echo "Enumerating subdomains for: $target"
-  
-  IFS=',' read -ra tool_list <<< "$tools"
-  
-  for tool in "${tool_list[@]}"; do
-    case "$tool" in
-      subfinder)
+for tool in "${tool_list[@]}"; do
+  case "$tool" in
+    subfinder)
+      if [ -n "$target" ]; then
         subfinder -d "$target" -silent >> "$filename"
         echo "[+] subfinder Done"
-        ;;
-      assetfinder)
+      elif [ -n "$list_file" ]; then
+        while read -r domain; do
+          subfinder -d "$domain" -silent >> "$filename"
+          echo "[+] subfinder Done for $domain"
+        done < "$list_file"
+      fi
+      ;;
+    assetfinder)
+      if [ -n "$target" ]; then
         assetfinder "$target" -subs-only | grep "$target" >> "$filename"
         echo "[+] assetfinder Done"
-        ;;
-      findomain)
+      elif [ -n "$list_file" ]; then
+        while read -r domain; do
+          assetfinder "$domain" -subs-only | grep "$domain" >> "$filename"
+          echo "[+] assetfinder Done for $domain"
+        done < "$list_file"
+      fi
+      ;;
+    findomain)
+      if [ -n "$target" ]; then
         findomain -t "$target" -q >> "$filename"
         echo "[+] findomain Done"
-        ;;
-      httprobe)
-        cat "$filename" | httprobe >> "$filename.tmp"
-        mv "$filename.tmp" "$filename"
-        echo "[+] httprobe Done"
-        ;;
-      sublist3r)
+      elif [ -n "$list_file" ]; then
+        while read -r domain; do
+          findomain -t "$domain" -q >> "$filename"
+          echo "[+] findomain Done for $domain"
+        done < "$list_file"
+      fi
+      ;;
+    httprobe)
+      cat "$filename" | httprobe >> "$filename.tmp"
+      mv "$filename.tmp" "$filename"
+      echo "[+] httprobe Done"
+      ;;
+    sublist3r)
+      if [ -n "$target" ]; then
         sublist3r -d "$target" -o "tmp-file.txt"
         cat "tmp-file.txt" >> "$filename"
         rm -rf "tmp-file.txt"
         echo "[+] sublist3r Done"
-        ;;
-      *)
-        echo "Unknown tool: $tool"
-        ;;
-    esac
-  done
+      elif [ -n "$list_file" ]; then
+        while read -r domain; do
+          sublist3r -d "$domain" -o "tmp-file.txt"
+          cat "tmp-file.txt" >> "$filename"
+          rm -rf "tmp-file.txt"
+          echo "[+] sublist3r Done for $domain"
+        done < "$list_file"
+      fi
+      ;;
+    *)
+      echo "Unknown tool: $tool"
+      ;;
+  esac
 done
 
 sort -u "$filename" -o "$filename"
