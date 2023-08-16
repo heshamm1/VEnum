8cba044a17eb2781785c7591695b1445b4069e91
Hesham Ahmed <87103294+heshamm1@users.noreply.github.com>
2023-08-16 18:00:41 +0300

    Update VEnum.sh

--- VEnum.sh
+++ VEnum.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# ANSI color codes
+BLUE='\e[34m'
+NC='\e[0m' # No Color
+
 
 banner="
 ██╗   ██╗  ███████╗███╗   ██╗██╗   ██╗███╗   ███╗
@@ -11,10 +15,10 @@ banner="
 "
 
 usage="
-Usage: $(basename "$0") -d TARGET [-f FILENAME] [-h] [-v] [--resolve IP_FILE] [--proxy PROXY] [--only TOOL1,TOOL2,...] [-a]
+Usage: $(basename "$0") -l TARGET_FILE [-f FILENAME] [-h] [-v] [--resolve IP_FILE] [--proxy PROXY] [--only TOOL1,TOOL2,...] [-a]
 
 Options:
-  -d       Target domain (required)
+  -l       File containing list of target domains (required)
   -f       Filepath and name to save results (default: subs.txt)
   -h       Show this help message and exit
   -v       Verbose output
@@ -26,13 +30,13 @@ Options:
 
 # Default values
 filename="subs.txt"
-tools="subfinder,assetfinder,findomain,amass,httprobe,dnsrecon,sublist3r"
+tools="subfinder,assetfinder,findomain,httprobe,sublist3r"
 verbose=false
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -d)
-      target="$2"
+    -l)
+      target_file="$2"
       shift
       shift
       ;;
@@ -78,51 +82,51 @@ done
 
 echo "$banner"
 
-if [ -z "$target" ]; then
-  echo "Error: Target domain is required. Use -h for help."
+if [ -z "$target_file" ]; then
+  echo "Error: Target file is required. Use -h for help."
   exit 1
 fi
 
 rm -f "$filename"
 touch "$filename"
 
-IFS=',' read -ra tool_list <<< "$tools"
+IFS=$'\r\n' GLOBIGNORE='*' command eval 'targets=($(cat "$target_file"))'
 
-for tool in "${tool_list[@]}"; do
-  case "$tool" in
-    subfinder)
-      subfinder -d "$target" -silent >> "$filename"
-      echo "[+] subfinder Done"
-      ;;
-    assetfinder)
-      assetfinder "$target" -subs-only | grep "$target" >> "$filename"
-      echo "[+] assetfinder Done"
-      ;;
-    findomain)
-      findomain -t "$target" -q >> "$filename"
-      echo "[+] findomain Done"
-      ;;
-    amass)
-      amass enum -d "$target" >> "$filename"
-      echo "[+] amass Done"
-      ;;
-    httprobe)
-      cat "$filename" | httprobe >> "$filename.tmp"
-      mv "$filename.tmp" "$filename"
-      echo "[+] httprobe Done"
-      ;;
-    dnsrecon)
-      dnsrecon -d "$target" >> "$filename"
-      echo "[+] dnsrecon Done"
-      ;;
-    sublist3r)
-      sublist3r -d "$target" >> "$filename"
-      echo "[+] sublist3r Done"
-      ;;
-    *)
-      echo "Unknown tool: $tool"
-      ;;
-  esac
+for target in "${targets[@]}"; do
+  echo "Enumerating subdomains for: $target"
+  
+  IFS=',' read -ra tool_list <<< "$tools"
+  
+  for tool in "${tool_list[@]}"; do
+    case "$tool" in
+      subfinder)
+        subfinder -d "$target" -silent >> "$filename"
+        echo "[+] subfinder Done"
+        ;;
+      assetfinder)
+        assetfinder "$target" -subs-only | grep "$target" >> "$filename"
+        echo "[+] assetfinder Done"
+        ;;
+      findomain)
+        findomain -t "$target" -q >> "$filename"
+        echo "[+] findomain Done"
+        ;;
+      httprobe)
+        cat "$filename" | httprobe >> "$filename.tmp"
+        mv "$filename.tmp" "$filename"
+        echo "[+] httprobe Done"
+        ;;
+      sublist3r)
+        sublist3r -d "$target" -o "tmp-file.txt"
+        cat "tmp-file.txt" >> "$filename"
+        rm -rf "tmp-file.txt"
+        echo "[+] sublist3r Done"
+        ;;
+      *)
+        echo "Unknown tool: $tool"
+        ;;
+    esac
+  done
 done
 
 sort -u "$filename" -o "$filename"
@@ -138,6 +142,5 @@ if [ -n "$resolve_file" ]; then
 fi
 
 if $verbose; then
-  echo "Tool execution complete."
+  echo -e "${BLUE}[^^] Enumeration Has Been Done.${NC}"
 fi
-
