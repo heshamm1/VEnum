1546531f55bc4f673ea0fcdee5b75ad6b7b6023b
Hesham Ahmed <87103294+heshamm1@users.noreply.github.com>
2023-08-16 22:18:29 +0300

    Update VEnum.sh

--- VEnum.sh
+++ VEnum.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 # ANSI color codes
 BLUE='\e[34m'
+RED='\e[91m'
+YELLOW='\e[93m'
 NC='\e[0m' # No Color
 
-
 banner="
-██╗   ██╗  ███████╗███╗   ██╗██╗   ██╗███╗   ███╗
-██║   ██║  ██╔════╝████╗  ██║██║   ██║████╗ ████║
-██║   ██║  █████╗  ██╔██╗ ██║██║   ██║██╔████╔██║
-╚██╗ ██╔╝  ██╔══╝  ██║╚██╗██║██║   ██║██║╚██╔╝██║
- ╚████╔╝██╗███████╗██║ ╚████║╚██████╔╝██║ ╚═╝ ██║
-  ╚═══╝ ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝
-	  Subdomain Enumeration Script
-"
+ ${BLUE}██╗   ██╗  ███████╗███╗   ██╗██╗   ██╗███╗   ███╗
+ ██║   ██║  ██╔════╝████╗  ██║██║   ██║████╗ ████║
+ ██║   ██║  █████╗  ██╔██╗ ██║██║   ██║██╔████╔██║
+ ╚██╗ ██╔╝  ██╔══╝  ██║╚██╗██║██║   ██║██║╚██╔╝██║
+  ╚████╔╝██╗███████╗██║ ╚████║╚██████╔╝██║ ╚═╝ ██║
+   ╚═══╝ ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝
+ 	  Subdomain Enumeration Script
+              ${RED}Created By. Sh1Vv
+${NC}"
 
 usage="
-Usage: $(basename "$0") [-d TARGET | -l FILENAME] [-f FILENAME] [-h] [-v] [--resolve IP_FILE] [--proxy PROXY] [--only TOOL1,TOOL2,...] [-a]
+${YELLOW}Usage: $(basename "$0") [-d TARGET | -l FILENAME] [-f FILENAME] [-h] [-v] [--resolve IP_FILE] [--proxy PROXY] [--only TOOL1,TOOL2,...] [-a]${NC}
 
 Options:
   -d       	Target domain
@@ -31,29 +33,26 @@ Options:
 
 # Default values
 filename="subs.txt"
-tools="subfinder,assetfinder,findomain,httprobe,sublist3r"
+tools="subfinder,assetfinder,findomain,httprobe"
 verbose=false
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     -d)
       target="$2"
-      shift
-      shift
+      shift 2
       ;;
     -l)
       list_file="$2"
-      shift
-      shift
+      shift 2
       ;;
     -f)
       filename="$2"
-      shift
-      shift
+      shift 2
       ;;
     -h)
-      echo "$banner"
-      echo "$usage"
+      echo -e "$banner"
+      echo -e "$usage"
       exit 0
       ;;
     -v)
@@ -62,39 +61,37 @@ while [[ $# -gt 0 ]]; do
       ;;
     --resolve)
       resolve_file="$2"
-      shift
-      shift
+      shift 2
       ;;
     --proxy)
       proxy="$2"
-      shift
-      shift
+      shift 2
       ;;
     --only)
       tools="$2"
-      shift
-      shift
+      shift 2
       ;;
     -a)
       shift
       ;;
     *)
-      echo "Unknown option: $1"
-      echo "$usage"
+      echo -e "${RED}Error: Unknown option: $1${NC}"
+      echo -e "$usage"
       exit 1
       ;;
   esac
 done
 
-echo "$banner"
+echo -e "$banner"
 
 if [ -z "$target" ] && [ -z "$list_file" ]; then
-  echo "Error: Either a target domain (-d) or a list of target domains (-l) is required. Use -h for help."
+  echo -e "${RED}Error: Either a target domain (-d) or a list of target domains (-l) is required. Use -h for help.${NC}"
   exit 1
 fi
 
 rm -f "$filename"
 touch "$filename"
+echo "[+] Script has been executed..."
 
 IFS=',' read -ra tool_list <<< "$tools"
 
@@ -102,59 +99,44 @@ for tool in "${tool_list[@]}"; do
   case "$tool" in
     subfinder)
       if [ -n "$target" ]; then
-        subfinder -d "$target" -silent >> "$filename"
+        subfinder -d "$target" -silent >> "$filename" || echo -e "${RED}[!] subfinder had an issue.${NC}"
         echo "[+] subfinder Done"
       elif [ -n "$list_file" ]; then
         while read -r domain; do
-          subfinder -d "$domain" -silent >> "$filename"
+          subfinder -d "$domain" -silent >> "$filename" || echo -e "${RED}[!] subfinder had an issue for $domain.${NC}"
           echo "[+] subfinder Done for $domain"
         done < "$list_file"
       fi
       ;;
     assetfinder)
       if [ -n "$target" ]; then
-        assetfinder "$target" -subs-only | grep "$target" >> "$filename"
+        assetfinder "$target" -subs-only | grep "$target" >> "$filename" || echo -e "${RED}[!] assetfinder had an issue.${NC}"
         echo "[+] assetfinder Done"
       elif [ -n "$list_file" ]; then
         while read -r domain; do
-          assetfinder "$domain" -subs-only | grep "$domain" >> "$filename"
+          assetfinder "$domain" -subs-only | grep "$domain" >> "$filename" || echo -e "${RED}[!] assetfinder had an issue for $domain.${NC}"
           echo "[+] assetfinder Done for $domain"
         done < "$list_file"
       fi
       ;;
     findomain)
       if [ -n "$target" ]; then
-        findomain -t "$target" -q >> "$filename"
+        findomain -t "$target" -q >> "$filename" || echo -e "${RED}[!] findomain had an issue.${NC}"
         echo "[+] findomain Done"
       elif [ -n "$list_file" ]; then
         while read -r domain; do
-          findomain -t "$domain" -q >> "$filename"
+          findomain -t "$domain" -q >> "$filename" || echo -e "${RED}[!] findomain had an issue for $domain.${NC}"
           echo "[+] findomain Done for $domain"
         done < "$list_file"
       fi
       ;;
     httprobe)
-      cat "$filename" | httprobe >> "$filename.tmp"
-      mv "$filename.tmp" "$filename"
+      cat "$filename" | httprobe >> "$filename.tmp" || echo -e "${RED}[!] httprobe had an issue.${NC}"
+      cat "$filename.tmp" >> "$filename"
       echo "[+] httprobe Done"
       ;;
-    sublist3r)
-      if [ -n "$target" ]; then
-        sublist3r -d "$target" -o "tmp-file.txt"
-        cat "tmp-file.txt" >> "$filename"
-        rm -rf "tmp-file.txt"
-        echo "[+] sublist3r Done"
-      elif [ -n "$list_file" ]; then
-        while read -r domain; do
-          sublist3r -d "$domain" -o "tmp-file.txt"
-          cat "tmp-file.txt" >> "$filename"
-          rm -rf "tmp-file.txt"
-          echo "[+] sublist3r Done for $domain"
-        done < "$list_file"
-      fi
-      ;;
     *)
-      echo "Unknown tool: $tool"
+      echo -e "${RED}Unknown tool: $tool${NC}"
       ;;
   esac
 done
@@ -172,5 +154,5 @@ if [ -n "$resolve_file" ]; then
 fi
 
 if $verbose; then
-  echo -e "${BLUE}[^^] Enumeration Has Been Done.${NC}"
+  echo -e "${BLUE}[^^] Enumeration has been completed.${NC}"
 fi
